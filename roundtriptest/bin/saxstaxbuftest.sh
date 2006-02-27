1200ebe9111abbddde6b4c4da31ebca925691aeb
sandoz <sandoz@localhost>
2006-02-27 15:11:58 +0000

    Updated scripts to include jar dependencies
    svn path=/trunk/; revision=79

--- roundtriptest/bin/saxstaxbuftest.sh
+++ roundtriptest/bin/saxstaxbuftest.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms384m -Xmx384m -cp ${XSB_HOME}/roundtriptest/lib/xom-1.1b4.jar:${XSB_HOME}/roundtriptest/lib/sjsxp.jar:${XSB_HOME}/streambuffer/lib/jsr173_api.jar:${XSB_HOME}/roundtriptest/lib/stax-utils.jar:${XSB_HOME}/streambuffer/dist/streambuffer.jar:${XSB_HOME}/roundtriptest/dist/roundtriptest.jar com.sun.xml.stream.buffer.test.SAXcreatedStAXprocessedComparator $1
+java -Xms384m -Xmx384m -cp ${XSB_HOME}/roundtriptest/lib/xom-1.1b4.jar:${XSB_HOME}/roundtriptest/lib/sjsxp.jar:${XSB_HOME}/streambuffer/lib/jsr173_api.jar:${XSB_HOME}/roundtriptest/lib/stax-utils.jar:${XSB_HOME}/streambuffer/dist/streambuffer.jar:${XSB_HOME}/streambuffer/lib/stax-ex.jar:${XSB_HOME}/roundtriptest/dist/roundtriptest.jar com.sun.xml.stream.buffer.test.SAXcreatedStAXprocessedComparator $1
