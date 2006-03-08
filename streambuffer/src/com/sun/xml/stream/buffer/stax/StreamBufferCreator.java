d1ced1ac2ec5efb31dd5304df3b7772c76fbce86
Kohsuke Kawaguchi <kohsuke@java.net>
2006-03-08 23:54:01 +0000

    removed the code that didn't belong here.
    svn path=/trunk/; revision=97

--- streambuffer/src/com/sun/xml/stream/buffer/stax/StreamBufferCreator.java
+++ streambuffer/src/com/sun/xml/stream/buffer/stax/StreamBufferCreator.java
@@ -2,9 +2,6 @@ package com.sun.xml.stream.buffer.stax;
 
 import com.sun.xml.stream.buffer.AbstractCreator;
 
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-
 /**
  * {@link AbstractCreator} with additional convenience code.
  *
@@ -13,8 +10,7 @@ import javax.xml.stream.XMLStreamReader;
  * @author Kohsuke Kawaguchi
  */
 abstract class StreamBufferCreator extends AbstractCreator {
-    
-    protected final void storeQualifiedName(int item, String prefix, String uri, String localName) {
+    protected void storeQualifiedName(int item, String prefix, String uri, String localName) {
         if (uri != null && uri.length() > 0) {
             if (prefix != null && prefix.length() > 0) {
                 item |= FLAG_PREFIX;
@@ -46,13 +42,13 @@ abstract class StreamBufferCreator extends AbstractCreator {
         storeStructure(item);
     }
 
-    protected final void storeAttribute(String prefix, String uri, String localName, String type, String value) throws XMLStreamException {
+    protected final void storeAttribute(String prefix, String uri, String localName, String type, String value) {
         storeQualifiedName(T_ATTRIBUTE_LN, prefix, uri, localName);
 
         storeStructureString(type);
         storeContentString(value);
     }
-    
+
     protected final void storeProcessingInstruction(String target, String data) {
         storeStructure(T_PROCESSING_INSTRUCTION);
         storeStructureString(target);
