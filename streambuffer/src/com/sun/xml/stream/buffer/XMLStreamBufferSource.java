9861cb9ea60c16248d1faedfd762af895feb2cc6
sandoz <sandoz@localhost>
2006-03-20 17:30:30 +0000

    Tidied up XMLStreamBufferSource/Result. Added some JavaDoc.
    svn path=/trunk/; revision=114

--- streambuffer/src/com/sun/xml/stream/buffer/XMLStreamBufferSource.java
+++ streambuffer/src/com/sun/xml/stream/buffer/XMLStreamBufferSource.java
@@ -21,36 +21,77 @@ package com.sun.xml.stream.buffer;
 
 import com.sun.xml.stream.buffer.sax.SAXBufferProcessor;
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import javax.xml.transform.sax.SAXSource;
 import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 
+/**
+ * A JAXP Source implementation that supports the parsing
+ * of {@link XMLStreamBuffer} for use by applications that expect a Source.
+ *
+ * <p>
+ * The derivation of XMLStreamBufferSource from SAXSource is an implementation
+ * detail.
+ *
+ * <p>Applications shall obey the following restrictions:
+ * <ul>
+ * <li>The setXMLReader and setInputSource shall not be called.</li>
+ * <li>The XMLReader object obtained by the getXMLReader method shall
+ *     be used only for parsing the InputSource object returned by
+ *     the getInputSource method.</li>
+ * <li>The InputSource object obtained by the getInputSource method shall 
+ *     be used only for being parsed by the XMLReader object returned by 
+ *     the getXMLReader method.</li>
+ * </ul>
+ */
 public class XMLStreamBufferSource extends SAXSource {
-    XMLStreamBuffer _buffer;
-    SAXBufferProcessor _bufferProcessor;
+    protected XMLStreamBuffer _buffer;
+    protected SAXBufferProcessor _bufferProcessor;
    
+    /**
+     * XMLStreamBufferSource constructor.
+     *
+     * @param buffer the {@link XMLStreamBuffer} to use.
+     */
     public XMLStreamBufferSource(XMLStreamBuffer buffer) {
         super(new InputSource(
                 new ByteArrayInputStream(new byte[0])));
         setXMLStreamBuffer(buffer);
     }
 
+    /**
+     * Get the {@link XMLStreamBuffer} that is used.
+     *
+     * @return the {@link XMLStreamBuffer}.
+     */
     public XMLStreamBuffer getXMLStreamBuffer() {
         return _buffer;
     }
     
+    /**
+     * Set the {@link XMLStreamBuffer} to use.
+     *
+     * @param buffer the {@link XMLStreamBuffer}.
+     */
     public void setXMLStreamBuffer(XMLStreamBuffer buffer) {
+        if (buffer == null) {
+            throw new NullPointerException("buffer cannot be null");
+        }        
         _buffer = buffer;
+        
+        if (_bufferProcessor != null) {
+            _bufferProcessor.setBuffer(_buffer);
+        }
     }
     
     public XMLReader getXMLReader() {
-        XMLReader reader = super.getXMLReader();
-        if (reader == null) {
-            reader = _bufferProcessor = new SAXBufferProcessor();
-            setXMLReader(reader);
+        if (_bufferProcessor == null) {
+            _bufferProcessor = new SAXBufferProcessor(_buffer);
+            setXMLReader(_bufferProcessor);
+        } else if (super.getXMLReader() == null) {
+            setXMLReader(_bufferProcessor);
         }
-        _bufferProcessor.setXMLStreamBuffer(_buffer);
-        return reader;
+
+        return _bufferProcessor;
     }        
 }
