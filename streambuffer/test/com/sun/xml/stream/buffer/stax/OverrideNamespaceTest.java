32dd003f1ca4066a34b0bca4ecd4edaa79af40ff
sandoz <sandoz@localhost>
2007-06-11 15:50:32 +0000

    Fleshed out unti test.
    svn path=/trunk/; revision=217

--- streambuffer/test/com/sun/xml/stream/buffer/stax/OverrideNamespaceTest.java
+++ streambuffer/test/com/sun/xml/stream/buffer/stax/OverrideNamespaceTest.java
@@ -17,16 +17,27 @@ public class OverrideNamespaceTest extends TestCase {
     }
 
     public void testOverrideNamespace() throws Exception {
-        String[] namespaces = {"http://wsdl", "http://types", "http://wsdl"};
+        String[] startElement = {"definitions", "types", "binding"};
+        String[] startElement_namespaces = {"http://wsdl", "http://types", "http://wsdl"};
+        String[] endElement = {"types", "binding", "definitions"};
+        String[] endElement_namespaces = {"http://types", "http://wsdl", "http://wsdl"};
         
         String str = "<tns:definitions xmlns:tns='http://wsdl'><tns:types xmlns:tns='http://types'/><tns:binding/></tns:definitions>";
         XMLStreamReader rdr = XMLInputFactory.newInstance().createXMLStreamReader(new StringReader(str));
         XMLStreamBuffer xsb = XMLStreamBuffer.createNewBufferFromXMLStreamReader(rdr);
         XMLStreamReader xsbrdr = xsb.readAsXMLStreamReader();
         int i = 0;
+        int j = 0;
         while(xsbrdr.hasNext()) {
-            if (xsbrdr.next() == XMLStreamReader.START_ELEMENT) {
-                assertEquals("Element " + xsbrdr.getNamespaceURI("tns") + " has wrong namespace", namespaces[i++], xsbrdr.getNamespaceURI("tns"));
+            int event = xsbrdr.next();
+            if (event == XMLStreamReader.START_ELEMENT) {
+                assertEquals("Start of Element " + xsbrdr.getName() + " has wrong namespace", 
+                        startElement_namespaces[i], xsbrdr.getNamespaceURI("tns"));
+                assertEquals(startElement[i++], xsbrdr.getLocalName());
+            } else if (event == XMLStreamReader.END_ELEMENT) {
+                assertEquals("End of Element " + xsbrdr.getName() + " has wrong namespace", 
+                        endElement_namespaces[j], xsbrdr.getNamespaceURI("tns"));
+                assertEquals(endElement[j++], xsbrdr.getLocalName());
             }
         }
     }
