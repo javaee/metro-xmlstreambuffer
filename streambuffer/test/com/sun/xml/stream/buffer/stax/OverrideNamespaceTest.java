284af12a59a4ba05480052ebcf09e13b54030219
jitendra kotamraju <jitu@java.net>
2007-06-11 23:43:12 +0000

    Adding a new testcase Issue number: Obtained from: Submitted by: Reviewed by:
    svn path=/trunk/; revision=221

--- streambuffer/test/com/sun/xml/stream/buffer/stax/OverrideNamespaceTest.java
+++ streambuffer/test/com/sun/xml/stream/buffer/stax/OverrideNamespaceTest.java
@@ -2,6 +2,7 @@ package com.sun.xml.stream.buffer.stax;
 
 import com.sun.xml.stream.buffer.XMLStreamBuffer;
 import java.io.StringReader;
+import java.util.*;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
 import junit.framework.*;
@@ -38,6 +39,33 @@ public class OverrideNamespaceTest extends TestCase {
                 assertEquals("End of Element " + xsbrdr.getName() + " has wrong namespace", 
                         endElement_namespaces[j], xsbrdr.getNamespaceURI("tns"));
                 assertEquals(endElement[j++], xsbrdr.getLocalName());
+            }
+        }
+    }
+
+    public void testOverrideNamespace1() throws Exception {
+        Map<String, String> ns = new LinkedHashMap();
+        ns.put("definitions", "http://wsdl");
+        ns.put("types", "http://types");
+        ns.put("binding", "http://wsdl");
+        ns.put("operation", "http://operation");
+        ns.put("port", "http://wsdl");
+
+        String str = "<tns:definitions xmlns:tns='http://wsdl'><tns:types xmlns:tns='http://types'/><tns:binding><tns:operation xmlns:tns='http://operation'/></tns:binding><tns:port/></tns:definitions>";
+
+        XMLStreamReader rdr = XMLInputFactory.newInstance().createXMLStreamReader(new StringReader(str));
+        XMLStreamBuffer xsb = XMLStreamBuffer.createNewBufferFromXMLStreamReader(rdr);
+        XMLStreamReader xsbrdr = xsb.readAsXMLStreamReader();
+        Iterator<String> nsItr = ns.keySet().iterator();
+        while(xsbrdr.hasNext()) {
+            int event = xsbrdr.next();
+            if (event == XMLStreamReader.START_ELEMENT) {
+		assertEquals("Wrong Start of Element", nsItr.next(), xsbrdr.getLocalName());
+                assertEquals("Start of Element " + xsbrdr.getName() + " has wrong namespace", 
+                        ns.get(xsbrdr.getLocalName()), xsbrdr.getNamespaceURI("tns"));
+            } else if (event == XMLStreamReader.END_ELEMENT) {
+                assertEquals("End of Element " + xsbrdr.getName() + " has wrong namespace", 
+                        ns.get(xsbrdr.getLocalName()), xsbrdr.getNamespaceURI("tns"));
             }
         }
     }
