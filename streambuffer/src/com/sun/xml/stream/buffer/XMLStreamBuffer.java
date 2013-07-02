17767f35228d60ea6b376938f345ecf196e6db56
Martin Grebac <martin.grebac@oracle.com>
2013-07-02 10:57:09 +0000

    reverting fbugs fix, mark it as excluded; to unblock us from security failures; the code is not nice but works - changing it should be supported by bigger test coverage
    svn path=/trunk/; revision=365

--- streambuffer/src/com/sun/xml/stream/buffer/XMLStreamBuffer.java
+++ streambuffer/src/com/sun/xml/stream/buffer/XMLStreamBuffer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2005-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -157,7 +157,7 @@ public abstract class XMLStreamBuffer {
      * <code>true</code> if the buffer has been created.
      */
     public final boolean isCreated() {
-        return (_structure.getArray()[0] & AbstractCreatorProcessor.TYPE_MASK)  != AbstractCreatorProcessor.T_END;
+        return _structure.getArray()[0] != AbstractCreatorProcessor.T_END;
     }
 
     /**
